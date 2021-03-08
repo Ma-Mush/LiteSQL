2fd176f588b171c6415914ad1deffa21f8d11e85
Ma-Mush <74294847+Ma-Mush@users.noreply.github.com>
2021-03-08 19:52:06 +0500

    Update __init__.py

--- LiteSQL/__init__.py
+++ LiteSQL/__init__.py
@@ -3,57 +3,60 @@
 # Гитхаб - github.com/Ma-Mush/LiteSQL
 # PypI - pypi.org/project/litesql/
 # =============================================================================
-
+import sqlite3
 class lsql():
-    import sqlite3
-    conn = ''
-    cursor = ''
-    def connect(file_name):
-        f = file_name[::-1]
-        if f[:4] == 'db.':
+    def __init__(self, file):
+        f = file[::-1]
+        if f[:4] == "db.":
             f = f[4:]
-            file_name = f[::-1]
-        global conn, cursor
-        import sqlite3
-        conn = sqlite3.connect(f"{file_name}.db")
-        cursor = conn.cursor()
-    def create(names, table="albums"):
-        global conn, cursor
-        cursor.execute(f"CREATE TABLE {table} ({names})")
-        conn.commit()
-    def insert_data(data_mass, len_title, table="albums"): 
-        global conn, cursor
-        titl = '?,' * (len_title - 1 )
-        titl += '?'
-        cursor.executemany(f"INSERT INTO {table} VALUES ({titl})", data_mass)
-        conn.commit()
-    def edit_data(title_last, last, title_new, new, table="albums"):
-        global conn, cursor
-        cursor.execute(f"UPDATE {table} SET {title_new} = '{new}' WHERE {title_last} = '{last}'")
-        conn.commit()   
-    def delete_data(name, title_name, table="albums"):
-        global conn, cursor
-        cursor.execute(f"DELETE FROM {table} WHERE {title_name} = '{name}'")
-    def select_data(name, title, row_factor=False, table="albums"):
-        global conn, cursor
-        cursor.execute(f"SELECT * FROM {table} WHERE {title}=?", [(name)])
+            file = f[::-1]
+        self.conn = sqlite3.connect(f"{file}.db")
+        self.cursor = self.conn.cursor()
+
+    def create(self, names, table="albums"):
+        self.cursor.execute(f"CREATE TABLE {table} ({names})")
+        self.conn.commit()
+        
+    def insert_data(self, data_mass, len_title, table="albums"): 
+        len_title = "?,"*(len_title-1) + "?"
+        self.cursor.executemany(f"INSERT INTO {table} VALUES ({len_title})", data_mass)
+        self.conn.commit()
+        
+    def edit_data(self, title_last, last, title_new, new, table="albums"):
+        self.cursor.execute(f"UPDATE {table} SET {title_new} = {new} WHERE {title_last} = {last}")
+        self.conn.commit()  
+        
+    def delete_data(self, name, title_name, table="albums"):
+        self.cursor.execute(f"DELETE FROM {table} WHERE {title_name} = {name}")
+        
+    def select_data(self, name, title, row_factor=False, table="albums"):
+        self.cursor.execute(f"SELECT * FROM {table} WHERE {title}=?", [(name)])
         if row_factor:
-            return cursor.fetchone()
+            return self.cursor.fetchone()
         else:
-            return cursor.fetchall()
-    def select_data_with_sort(type_sort, name, title_name, table="albums"):
-        global conn, cursor
+            return self.cursor.fetchall()
+        
+    def select_data_with_sort(self, type_sort, name, title_name, table="albums"):
         a = []
         if name != None:
-            for row in cursor.execute(f"SELECT {type_sort}, * FROM {table} ORDER BY {title_name}", [(name)]):
+            for row in self.cursor.execute(f"SELECT {type_sort}, * FROM {table} ORDER BY {title_name}", [(name)]):
                 a.append(row)
             return a
         else:
-            for row in cursor.execute(f"SELECT {type_sort}, * FROM {table} ORDER BY {title_name}"):
+            for row in self.cursor.execute(f"SELECT {type_sort}, * FROM {table} ORDER BY {title_name}"):
                 a.append(row)
             return a
-    def search(type_search, name_search, table="albums"):
-        global conn, cursor
-        sql = f"SELECT * FROM {table} WHERE {type_search} LIKE '{name_search}'"
-        cursor.execute(sql)
-        return cursor.fetchall()
+        
+    def search(self, type_search, name_search, table="albums"):
+        self.cursor.execute(f"SELECT * FROM {table} WHERE {type_search} LIKE {name_search}")
+        return self.cursor.fetchall()
+    
+    def get_all_data(self, table="albums"):
+        self.cursor.execute(f"SELECT * FROM {table}")
+        return self.cursor.fetchall()
+    
+    def get_cursor(self):
+        return self.cursor
+    
+    def get_connect(self):
+        return self.conn
