2093022bef6726a11a1bda8a06d0332ede1ba0a0
Ma-Mush <74294847+Ma-Mush@users.noreply.github.com>
2024-06-29 18:23:15 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
 long_description1 = '''
+# LiteSQL 
+База данных? Без проблем!
+
+# ЧИТАТЬ НА GITHUB
 https://github.com/Ma-Mush/LiteSQL
+
 '''
 
 setup(
 name='LiteSQL', 
-version='1.0',
+version='2.0.2',
 description='Библиотека для легкого использования БД!', 
 packages=['LiteSQL'], 
 author_email='ma_mush@mail.ru', 
